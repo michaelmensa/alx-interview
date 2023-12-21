8aa1d3a99e12d8707241f7913ed2acad04152465
michaelmensa <michaelmensa@live.com>
2023-12-21 16:05:20 +0000

    log parsing[x]

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -6,50 +6,30 @@ import sys
 from collections import Counter
 
 
-def process_line(line, file_sizes, status_codes):
-    ''' function to process lines from stdin '''
-    strip_line = line.strip()
-    parsed_line = strip_line.split(' ')
-
-    file_sizes.append(int(parsed_line[-1]))
-    status_codes.append(int(parsed_line[-2]))
-
-
-def print_metrics(file_sizes, status_codes):
-    ''' prints metrics to the terminal '''
-    if file_sizes:
-        total_size = sum(file_sizes)
-        print(f'File_size: {total_size}')
-
-    occ = Counter(status_codes)
-    for value, count in sorted(occ.items()):
-        print(f'{value}: {count}')
-
-
-"""
-def main():
-    ''' reads line by line in stdin and return a list '''
-    file_sizes = []
-    status_codes = []
-    count = 0
-
-    try:
-        for line in sys.stdin:
-            count += 1
-            process_line(line, file_sizes, status_codes)
-
-            if count % 10 == 0:
-                print_metrics(file_sizes, status_codes)
-
-    except KeyboardInterrupt:
-        print_metrics(file_sizes, status_codes)
-        raise
-"""
-
 if __name__ == "__main__":
     file_sizes = []
     status_codes = []
     count = 0
+
+    def process_line(line, file_sizes, status_codes):
+        ''' function to process lines from stdin '''
+        strip_line = line.strip()
+        parsed_line = strip_line.split(' ')
+
+        file_sizes.append(int(parsed_line[-1]))
+        status_codes.append(int(parsed_line[-2]))
+
+
+    def print_metrics(file_sizes, status_codes):
+        ''' prints metrics to the terminal '''
+        if file_sizes:
+            total_size = sum(file_sizes)
+            print(f'File_size: {total_size}')
+
+        occ = Counter(status_codes)
+        for value, count in sorted(occ.items()):
+            print(f'{value}: {count}')
+
 
     try:
         for line in sys.stdin:
