5d0a16240f89e23d1b518da5002b5a4e63aadf96
michaelmensa <michaelmensa@live.com>
2023-12-21 12:11:32 +0000

    log parsingi

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -6,35 +6,40 @@ import sys
 from collections import Counter
 
 
+def process_line(line, file_sizes, status_codes):
+    ''' function to process lines from stdin '''
+    strip_line = line.strip()
+    parsed_line = strip_line.split(' ')
+
+    file_sizes.append(int(parsed_line[-1]))
+    status_codes.append(int(parsed_line[-2]))
+
+
+def print_metrics(file_sizes, status_codes):
+    ''' prints metrics to the terminal ''' 
+    if file_sizes:
+        total_size = sum(file_sizes)
+        print(f'File_size: {total_size}')
+
+    occ = Counter(status_codes)
+    for value, count in sorted(occ.items()):
+        print(f'{value}: {count}')
+
+
 def main():
     ''' reads line by line in stdin and return a list '''
-    stats = []
-    file_size = []
-    status_code = []
-    stats.append(file_size)
-    stats.append(status_code)
-    lines = 0
+    file_sizes = []
+    status_codes = []
 
     try:
-        for line in sys.stdin:
-            lines += 1
-            strip_line = line.strip()
-            parsed_line = strip_line.split(' ')
+        for i, line in enumerate(sys.stdin, start=1):
+            process_line(line, file_sizes, status_codes)
 
-            file_size.append(int(parsed_line[-1]))
-            status_code.append(int(parsed_line[-2]))
-
-            if lines % 10 == 0:
-                if len(file_size) > 0:
-                    total_size = sum(file_size)
-                    print(f'File_size: {total_size}')
-
-                occ = Counter(status_code)
-                for value, count in sorted(occ.items()):
-                    print(f'{value}: {count}')
+            if i % 10 == 0:
+                print_metrics(file_sizes, status_codes)
 
     except KeyboardInterrupt:
-        pass
+        print_metrics(file_sizes, status_codes)
 
 if __name__ == "__main__":
     main()
